cabeba987d8fd34f460765cfa9ae89c78d80bfd5
Amoo1187 <Moondra.Aarnav@gmail.com>
2024-10-19 18:45:19 -0400

    Update upload.js

--- src/upload.js
+++ src/upload.js
@@ -1,18 +1,22 @@
 // FileUpload.js
 import React, { useState } from 'react';
+import { Button, Alert, Container, Row, Col, Form } from 'react-bootstrap';
 
 const FileUpload = () => {
   const [file, setFile] = useState(null);
   const [message, setMessage] = useState('');
+  const [variant, setVariant] = useState('');
 
   // Handle file selection
   const onFileChange = (event) => {
     setFile(event.target.files[0]);
+    setMessage('');
   };
 
   // Handle file upload
   const onFileUpload = () => {
     if (!file) {
+      setVariant('danger');
       setMessage('Please select a file first.');
       return;
     }
@@ -33,12 +37,33 @@ const FileUpload = () => {
   };
 
   return (
-    <div>
-      <h2>File Upload Page</h2>
-      <input type="file" onChange={onFileChange} />
-      <button onClick={onFileUpload}>Upload</button>
-      {message && <p>{message}</p>}
-    </div>
+    <Container className="mt-5">
+    <Row className="justify-content-center">
+    <Col md={6}>
+    <div className="text-center">
+            <h2 className="mb-4">File Upload</h2>
+          </div>
+
+          <Form>
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Control type="file" onChange={onFileChange} />
+            </Form.Group>
+            <div className="d-grid gap-2">
+              <Button variant="primary" onClick={onFileUpload} size="lg">
+                Upload
+              </Button>
+            </div>
+          </Form>
+
+          {/* Display message with conditional rendering */}
+          {message && (
+            <Alert className="mt-4 text-center" variant={variant}>
+              {message}
+            </Alert>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
